16c697cd520e18de46f5c131b919ec70eca4696c
Morten Anton Bach Sjøgren <m_abs@mabs.dk>
2020-02-04 12:04:43 +0100

    add simple drawer

--- lib/screens/publication-renderer.dart
+++ lib/screens/publication-renderer.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,15 +18,26 @@ class PublicationRendererState extends State<PublicationRenderer> {
       future: fetchWebPublication(),
       builder: (context, AsyncSnapshot<http.Response> snapshot) {
         if (snapshot.hasData) {
+          dynamic webPub = json.decode(snapshot.data.body);
+          dynamic metadata = webPub['metadata'];
+          dynamic title = metadata['title'];
+
+          dynamic readingOrder = webPub['readingOrder'];
+          dynamic resources = webPub['resources'];
+          dynamic toc = webPub['toc'];
+          dynamic pageList = webPub['page-list'];
+          dynamic landmarks = webPub['landmarks'];
+          print(json.encode(toc));
           return Scaffold(
             appBar: AppBar(
               // Here we take the value from the MyHomePage object that was created by
               // the App.build method, and use it to set our appbar title.
-              title: Text('Loaded'),
+              title: Text(title),
             ),
             body: Center(
               child: Text('Loaded'),
             ),
+            drawer: _makeDrawer(context, webPub),
           );
         }
 
@@ -40,6 +53,62 @@ class PublicationRendererState extends State<PublicationRenderer> {
         );
       },
     );
+  }
+
+  Widget _makeDrawer(context, dynamic webPub) {
+    List<Widget> children = [
+      DrawerHeader(
+        child: Text('Drawer Header'),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+        ),
+      ),
+      Semantics(
+        header: true,
+        child: ListTile(
+          title: Text(
+            'Table of Content',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      )
+    ];
+
+    children.addAll(_makeToc(context, webPub['toc'], 0));
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: children,
+      ),
+    );
+  }
+
+  List<Widget> _makeToc(context, dynamic tocItems, double depth) {
+    List<Widget> tocWidgets = List<Widget>();
+    for (var i = 0; i < tocItems.length; i += 1) {
+      dynamic tocItem = tocItems[i];
+      tocWidgets.add(ListTile(
+        title: Text(tocItem['title']),
+        contentPadding: EdgeInsets.fromLTRB(
+          16 * (depth + 1),
+          16,
+          16,
+          16,
+        ),
+        onTap: () {
+          print(tocItem['href']);
+
+          Navigator.pop(context);
+        },
+      ));
+
+      if (tocItem['children'] != null) {
+        tocWidgets.addAll(_makeToc(context, tocItem['children'], depth + 1));
+      }
+    }
+
+    return tocWidgets;
   }
 
   Future<http.Response> fetchWebPublication() async {
